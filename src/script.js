048c9f410e02eaabb73350b3bb592ddb73040dce
Patrick-home <lucas.patrick.lsilva@gmail.com>
2021-11-26 00:13:10 -0300

    dark-mode

---
+++ src/script.js
@@ -0,0 +1,25 @@
+// Dark mode
+
+const darkMode = document.querySelector(`#button-mode`).addEventListener(`click`, () => {
+    const modePage = document.querySelector(`.container`)
+    modePage.classList.toggle(`active`)
+})
+
+
+
+// Show Password
+
+
+
+// const pass = document.querySelector(`#password`)
+
+// pass.addEventListener(`focus`, () => {
+    
+// })
+
+
+
+// const showPass = document.querySelector(`#show-pass`)
+//     showPass.classList.toggle(`eye`)
+//     let conteudo = document.querySelector(`.eye`)
+//     conteudo.innerHTML = `Mostrar`
